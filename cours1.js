0abee4d56432dcd219c4a646206a240ece263ba1
ecdev57 <enzociano57@gmail.com>
2018-01-08 17:03:36 +0100

    test type, rendu

--- cours1.js
+++ cours1.js
@@ -27,11 +27,11 @@ function urlTable(item){
 var maListe = [];
 maListe = Liste;
 
-console.log(maListe);
+//console.log(maListe);
 // step 2 : trouver le bon container
 var monUl = document.getElementById("listeContainer");
 //console.log(monUl);
-console.log(maListe.length);
+//console.log(maListe.length);
 // step3 : parcourir les données et effectuer le traitement sur chaque getElementById
 // option 1 (boucle), ForEach
 maListe.forEach(function(item) {
@@ -45,3 +45,29 @@ maListe.forEach(function(item) {
 	})	
 	monUl.appendChild(monLi);
 });
+var submit = document.getElementById("form_id");
+//execute la fonction au submit sur id submit.
+//submit sur form  appel click
+submit.addEventListener("submit", function(event){
+	//on stop la propagation
+	event.preventDefault();
+	var newuser{};
+	console.log(submit.elements);
+	//boucle sur les valeurs des  champs du formulaire et on exclud le bouton submit
+	for (var i =0; i< submit.length; i++) {
+		if (submit.elements[i].value !== "Submit") {
+			console.log(submit.elements[i].value);
+			newuser[submit.elements[i].name]=submit.elements[i].value;
+			maListe
+		}
+	}
+ 	//console.log(document.getElementById("Submit").value);
+
+  	/*var js = document.getElementById("js").value;
+  	var sitepef = document.getElementById("sitepef").value;
+  	var comsitepef = document.getElementById("comsitepef").value;
+  	var apppef = document.getElementById("apppef").value;
+  	var comapppef = document.getElementById("comapppef").value;
+  	var paravifa = document.getElementById("paravifa").value;
+  	var prvifa = document.getElementById("prvifa").value;*/
+})
